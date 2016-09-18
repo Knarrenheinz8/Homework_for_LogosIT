075e8c3666cb3b7d9cd77c95a703e4311ef4e847
Vitaliy <vezdexod1997@gmail.com>
2016-09-18 13:40:15 +0300

    Added initial structure

---
+++ Homework/Homework.TaxiCourse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.TaxiCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
